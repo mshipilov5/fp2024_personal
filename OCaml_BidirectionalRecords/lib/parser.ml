b5f4ca677c2a9186eb68541131d8071bc3dccea7
mshipilov5 <shipilovmaksim05@gmail.com>
2024-10-01 02:39:18 +0300

    shipilovmaksim05@gmail.com  add basic parser for keywords, constants and variables

--- OCaml_BidirectionalRecords/lib/parser.ml
+++ OCaml_BidirectionalRecords/lib/parser.ml
@@ -1,65 +1,138 @@
-(** Copyright 2021-2023, Kakadu and contributors *)
+(** Copyright 2021-2024, mshipilov5 *)
 
 (** SPDX-License-Identifier: LGPL-3.0-or-later *)
 
-(* TODO: implement parser here *)
-open Angstrom
 
-let is_space = function
-  | ' ' | '\t' | '\n' | '\r' -> true
+open Angstrom
+open Base
+open Ast
+
+
+
+let is_keyword = function
+  | "let" | "in" | "fun" | "rec" | "if" | "then" | "else" |"true" | "false" -> true
   | _ -> false
 ;;
 
-let spaces = skip_while is_space
+
+let pws = take_while Char.is_whitespace
+let pstoken s = pws *> string s
+let ptoken s = pws *> s
+let pparens p = pstoken "(" *> p <* pstoken ")"
+
+
+let chain e op =
+  let rec go acc = lift2 (fun f x -> f acc x) op e >>= go <|> return acc in
+  e >>= go
+;;
+
+
+let pint =
+  let sign = choice [ pstoken "-"; pstoken "+"; pstoken "" ] in
+  let rest = take_while1 Char.is_digit in
+  lift2 (fun sign rest -> Int.of_string (sign ^ rest)) sign rest >>| fun x -> Int x
+;;
+let pbool =
+  choice [ pstoken "true" *> return true; pstoken "false" *> return false ]
+  >>| fun x -> Bool x
+;;
+let pstr = char '"' *> take_till (Char.equal '"') <* char '"' >>| fun x -> String x
+let punit = pstoken "()" >>| fun _ -> Unit
+
+let const = choice [ pint; pbool; pstr; punit ]
+
 
 let varname =
-  satisfy (function
-    | 'a' .. 'z' -> true
-    | _ -> false)
-;;
-
-let conde = function
-  | [] -> fail "empty conde"
-  | h :: tl -> List.fold_left ( <|> ) h tl
-;;
-
-type dispatch =
-  { apps : dispatch -> string Ast.t Angstrom.t
-  ; single : dispatch -> string Ast.t Angstrom.t
-  }
-
-type error = [ `Parsing_error of string ]
-
-let pp_error ppf = function
-  | `Parsing_error s -> Format.fprintf ppf "%s" s
-;;
-
-let parse_lam =
-  let single pack =
-    fix (fun _ ->
-      conde
-        [ char '(' *> pack.apps pack <* char ')' <?> "Parentheses expected"
-        ; ((string "λ" <|> string "\\") *> spaces *> varname
-           <* spaces
-           <* (return () <* char '.' <|> string "->" *> return ())
-           >>= fun var ->
-           pack.apps pack >>= fun b -> return (Ast.Abs (String.make 1 var, b)))
-        ; (varname <* spaces >>= fun c -> return (Ast.Var (String.make 1 c)))
-        ])
+  let pfirst =
+    satisfy (fun ch -> Char.is_alphanum ch || Char.equal ch '_') >>| Char.escaped
   in
-  let apps pack =
-    many1 (spaces *> pack.single pack <* spaces)
-    >>= function
-    | [] -> fail "bad syntax"
-    | x :: xs -> return @@ List.fold_left (fun l r -> Ast.App (l, r)) x xs
+  let prest =
+    take_while (fun ch -> Char.is_alphanum ch || Char.is_digit ch || Char.equal ch '_')
   in
-  { single; apps }
+  let varname = lift2 (fun x y -> x ^ y) pfirst prest in
+  ptoken varname
+  >>= fun s ->
+  if is_keyword s then fail "Unsupported keyword has been introduced" else return s
 ;;
 
-let parse str =
-  match
-    Angstrom.parse_string (parse_lam.apps parse_lam) ~consume:Angstrom.Consume.All str
-  with
-  | Result.Ok x -> Result.Ok x
-  | Error er -> Result.Error (`Parsing_error er)
+
+let pvar = varname >>| fun x -> PVar x
+let pconst = const >>| fun x -> PConst x
+
+let pEconst = const >>| fun x -> Econst x
+let pEvar = varname >>| fun x -> Evar x
+let peapp e = chain e (return (fun e1 e2 -> Efun_application (e1, e2)))
+
+
+
+let ptint = pstoken "int" *> return (Int 0)
+let ptstring = pstoken "string" *> return (String "")
+let ptbool = pstoken "bool" *> return (Bool false)
+let pty = choice [ ptint; ptstring; ptbool ]
+
+
+let parsebinop op token =
+  pws *> pstoken token *> return (fun e1 e2 -> Ebin_op (op, e1, e2))
 ;;
+
+
+let add = parsebinop Add "+"
+let sub = parsebinop Sub "-"
+let mult = parsebinop Mult "*"
+let div = parsebinop Div "/"
+
+
+let pbranch pexpr =
+  lift3
+    (fun e1 e2 e3 -> Eif_then_else (e1, e2, Some e3))
+    (pstoken "if" *> pexpr)
+    (pstoken "then" *> pexpr)
+    (pstoken "else" *> pexpr)
+;;
+
+
+let rel =
+  choice
+    [ parsebinop Eq "="
+    ; parsebinop Neq "<>"
+    ; parsebinop Lt "<"
+    ; parsebinop Gt ">"
+    ; parsebinop Lte "<="
+    ; parsebinop Gte ">="
+    ]
+;;
+
+
+let ppattern =
+  choice
+    [pconst; pvar]
+;;
+
+
+let plet pexpr =
+  let rec pbody pexpr =
+    ppattern >>= fun id -> pbody pexpr <|> pstoken "=" *> pexpr >>| fun e -> Efun ([id], e)
+  in
+  pstoken "let"
+  *> lift4
+       (fun is_rec id e1 e2 -> 
+          match is_rec with
+          | Rec -> Elet_rec (id, e1, e2)
+          | NonRec -> Elet (id, e1, e2))
+       (pstoken "rec" *> return Rec <|> return NonRec)
+       (pparens varname <|> varname)
+       (pstoken "=" *> pexpr <|> pbody pexpr)
+;;
+
+
+let pexpr =
+  fix (fun expr ->
+    let expr = choice [ pEconst; pEvar; pparens expr] in
+    let expr = peapp expr <|> expr in
+    let expr = chain expr (mult <|> div) in
+    let expr = chain expr (add <|> sub) in
+    let expr = chain expr rel in
+    let expr = pbranch expr <|> expr in
+    let expr = plet expr <|> expr in
+    expr)
+  ;;
